b20413b2bf083a43d162a3983fddbc47f61bf654
Argha Bhattacharjee <arghabhattacharjee999@gmail.com>
2026-01-20 20:45:05 +0530

    added silver layer macros

--- dbt_project/models/silver/silver_returns_info.sql
+++ dbt_project/models/silver/silver_returns_info.sql
@@ -19,11 +19,21 @@ cust as(
     select customer_sk,
             gender
     from {{ ref("bronze_customer") }}
-)
-
-
+),
+joined_data as (
 select * 
 from returns
 join sales on returns.sales_id = sales.sales_id
 join prod on returns.product_sk = prod.product_sk
-join cust on sales.customer_sk = cust.customer_sk;
+join cust on sales.customer_sk = cust.customer_sk
+)
+
+select
+       category,
+       sum(gross_amount) as gross_amount,
+       sum(refund_amount) as refund_amount,
+       {{ calculate_percentage("sum(refund_amount)", "sum(gross_amount)") }} as refund_percentage
+
+from joined_data
+group by 1
+order by 2,3
