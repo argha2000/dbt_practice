f3fab74e613ed0766a5bf312f70eee3c2bc26a65
Argha Bhattacharjee <arghabhattacharjee999@gmail.com>
2026-01-21 10:17:22 +0530

    added returns model

--- dbt_project/macros/calculate_percentage.sql
+++ dbt_project/macros/calculate_percentage.sql
@@ -1,6 +1,6 @@
-{{% macro calculate_percentage(col1,col2) %}}
+{% macro calculate_percentage(col1,col2) %}
     CASE 
         WHEN {{ col2 }} = 0 THEN 0
         ELSE ({{ col1 }} / {{ col2 }}) * 100
     END
-{{% endmacro %}}    
+{% endmacro %}
