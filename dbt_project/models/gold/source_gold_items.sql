1871e95e0a9a70b326f47ba34cf4e7e35197001c
Argha Bhattacharjee <argha.bhattacharjee@ibm.com>
2026-01-28 15:39:30 +0530

    fixed the issue facing while running source_gold_items.sql

--- dbt_project/models/gold/source_gold_items.sql
+++ dbt_project/models/gold/source_gold_items.sql
@@ -1,10 +1,9 @@
 with dedupe_query as(
 select *,
-row_number() over(partiton by id order by date_of_addition desc) as 
-FROM {{ ref('source','items')}}
-where dedupe_key = 1
+(row_number() over(partition by product_sk order by date_of_addition desc)) as dedupe_key
+FROM {{ source('source','items') }}
 )
 
-select id,name,category,date_of_addition
+select product_sk,product_name,category,date_of_addition
 from dedupe_query
 where dedupe_key = 1
