3a88bd65fcf69f837a38c2a6b3b9990b34b9b745
Argha Bhattacharjee <arghabhattacharjee999@gmail.com>
2026-01-29 12:35:48 +0530

    completed snapshots

--- dbt_project/models/gold/source_gold_items.sql
+++ dbt_project/models/gold/source_gold_items.sql
@@ -1,9 +1,14 @@
+
 with dedupe_query as(
 select *,
 (row_number() over(partition by product_sk order by date_of_addition desc)) as dedupe_key
 FROM {{ source('source','items') }}
 )
 
-select product_sk,product_name,category,date_of_addition
+select 
+    product_sk,
+   product_name,
+   category,
+ date_of_addition
 from dedupe_query
 where dedupe_key = 1
